4aef38f94cafa3bf7650df912e291478197534d9
Ewa <ewa.markowska3@gmail.com>
2023-02-28 08:49:52 +0100

    Login fixed empty response

--- Api/Controllers/AccountController.cs
+++ Api/Controllers/AccountController.cs
@@ -11,53 +11,48 @@ namespace Api.Controllers
     [Route("api/account")]
     [ApiController]
     public class AccountController : ControllerBase
-
     {
-
         private readonly IAccountServices _accountServices;
+
         public AccountController(IAccountServices accountServices)
         {
             _accountServices = accountServices;
-
         }
+
         [HttpPost("register")]
         public async Task<IActionResult> RegisterUser([FromBody] RegisterDto dto)
         {
             _accountServices.RegisterUser(dto);
             return StatusCode(200);
         }
-       
 
         [HttpPost("login")]
-
         public async Task<IActionResult> Login(LoginDto dto)
         {
-
             var user = _accountServices.AutenticateUser(dto);
+
             if (user == null)
             {
-                    return Unauthorized();
+                return Unauthorized();
+            }
 
-                }
-                var claimsOfferent = new List<Claim>
+            var claimsOfferent = new List<Claim>
             {
                 new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
                 new Claim(ClaimTypes.Name, user.Name),
                 new Claim(ClaimTypes.Role,$"{user.Role}"),
-
             };
 
-                var identityOfferent = new ClaimsIdentity(claimsOfferent, CookieAuthenticationDefaults.AuthenticationScheme);
-                var principalOfferent = new ClaimsPrincipal(identityOfferent);
+            var identityOfferent = new ClaimsIdentity(claimsOfferent, CookieAuthenticationDefaults.AuthenticationScheme);
+            var principalOfferent = new ClaimsPrincipal(identityOfferent);
 
-                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principalOfferent,
-                    new AuthenticationProperties { IsPersistent = dto.RememberLogin });
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principalOfferent, new AuthenticationProperties { IsPersistent = dto.RememberLogin });
 
-                return Ok();
+            Console.WriteLine("Logowanie się udało");
 
-            }
-           
- 
+            return Ok(user);
+        }
+
         [Authorize]
         [HttpPost("logout")]
         public async Task<IActionResult> Logout()
@@ -66,7 +61,5 @@ namespace Api.Controllers
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return LocalRedirect("/");
         }
-
     }
 }
-
